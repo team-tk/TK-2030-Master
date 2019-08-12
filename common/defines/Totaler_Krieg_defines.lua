a0d672c5a428f4d65c3b01908ead80a0727a891f
TK2030 <51347516+TK2030@users.noreply.github.com>
2019-08-12 11:32:50 -0500

    Initial Upload
    Intial Upload

---
+++ common/defines/Totaler_Krieg_defines.lua
@@ -0,0 +1,26 @@
+
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 30			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1				-- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 0			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 6					-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
+
+
+NDefines.NGame.END_DATE = "2042.1.1.1"
+
+NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
+NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 4000.0
+
+
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2;
+NDefines.NCountry.NUCLEAR_BOMB_DROP_WAR_SUPPORT_EFFECT_MAX_INFRA = 10.0; 
+NDefines.NCountry.NUCLEAR_BOMB_DROP_WAR_SUPPORT_EFFECT_MAX_VP = 10;
+NDefines.NMilitary.NUKE_DELAY_HOURS = 5;
+NDefines.NMilitary.NUKE_MIN_DAMAGE_PERCENT = 0.7;
+NDefines.NMilitary.NUKE_MAX_DAMAGE_PERCENT = 0.9;
+NDefines.NCountry.NUCLEAR_PRODUCTION_SCALE = 3900;
+
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 100
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 60
